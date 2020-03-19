dc7e775ff1248dfab6b2cadf6e97df738d2f0db2
kaoskar1 <oscar.ljungdahl@hotmail.com>
2020-03-19 13:45:05 +0100

    about start

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Vårt Kaffe</h1>
+    <h4>bala bla bla bla</h4>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus et ex in repellendus, nostrum quas voluptas 
+      sapiente maiores sequi consectetur ipsam.
+       Neque, pariatur adipisci et sed quod quam ipsa minima?</p>
+      
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quidem repellat
+         possimus, beatae vero rem in distinctio tenetur labore dolorum repudiandae 
+        id nihil placeat exercitationem molestiae numquam laboriosam praesentium? Dolorem.</p>
+    <img class="eva" src="../assets/graphics/eva-cortado.jpg" alt="Eva Cortado">
+    <h2>Eva Cortado</h2>
+    <h5>VD & Grundare</h5>
   </div>
 </template>
 
@@ -10,6 +22,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+body{
+  background: #f3e4e1;
+}
+.about{
+  text-align: center;
+  padding: 10px;
+  line-height: 1.5em;
+  width: 50%;
+}
 
+h4{
+  font-weight: bold;
+}
+
+.eva{
+  border-radius: 50%;
+}
 </style>
