3cbf277dd3a1ed6569bbfd7355b740702f4d676d
maxaakre <maxaakre@gmail.com>
2020-08-19 15:38:40 +0200

    refacture and deleted styles

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,50 @@
 <template>
   <div id="app">
-  <Navbar v-if="showNavbar"/>
-  <router-view/>
+    <Navbar v-if="showNavbar" />
+    <router-view />
   </div>
 </template>
 <script>
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 export default {
-  name:'App',
-  components:{
-    Navbar
+  name: "App",
+  components: {
+    Navbar,
   },
-  computed:{
-    showNavbar(){
-      return this.$store.state.showNavbar
-    }
+  computed: {
+    showNavbar() {
+      return this.$store.state.showNavbar;
+    },
   },
   //before mount is used to fetch the data from the menu json file
-  beforeMount(){
-    this.$store.dispatch('findUuid')
-    this.$store.dispatch('getMenuList')
-  }
-}
+  beforeMount() {
+    this.$store.dispatch("findUuid");
+    this.$store.dispatch("getMenuList");
+  },
+};
 </script>
 
 <style lang="scss">
 :root {
-    @import url('https://fonts.googleapis.com/css?family=PT+Serif:700|Work+Sans:400,600&display=swap');
+  @import url("https://fonts.googleapis.com/css?family=PT+Serif:700|Work+Sans:400,600&display=swap");
 }
-*{
+* {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
 }
 #app {
-  font-family:'Work Sans';
+  font-family: "Work Sans";
   text-align: center;
-  color: #38846D;
-  background: #F3E4E1;
+  color: #38846d;
+  background: #f3e4e1;
   box-sizing: border-box;
   max-width: 480px;
-  margin: 0; 
-  
+  margin: 0 auto;
+}
+@media screen and (min-width: 1024px) {
+  #app {
+    overflow: scroll;
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,144 +1,144 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
+const API = "http://localhost:5000/api/beans";
 
-const API = 'http://localhost:5000/api/beans'
-
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     activeOrder: {},
-    loading: false, 
-    menu:[],
-    showNavbar:false,
-    cart:[],
-    orderHistory:[],
-    user:{name: '', email:''}
-
+    loading: false,
+    menu: [],
+    showNavbar: false,
+    cart: [],
+    orderHistory: [],
+    user: { name: "", email: "" },
   },
   mutations: {
-    login(state,data){
-    state.user = data  
+    login(state, data) {
+      state.user = data;
     },
-    displayMenu(state,menu){
-      state.menu = menu
+    displayMenu(state, menu) {
+      state.menu = menu;
     },
-    closeNavbar(state){
-      state.showNavbar = !state.showNavbar
+    closeNavbar(state) {
+      state.showNavbar = !state.showNavbar;
     },
-    additem(state,item){
+    additem(state, item) {
       state.cart.push({
-        id:item.id,
-        price:item.price,
-        title:item.title,
-        quantity:1
-      })
+        id: item.id,
+        price: item.price,
+        title: item.title,
+        quantity: 1,
+      });
     },
-    orderStatus(state,order){
-      console.log("här kommer ordern")
+    orderStatus(state, order) {
+      console.log("här kommer ordern");
       console.log(order);
-      state.activeOrder = order
+      state.activeOrder = order;
     },
-    emptyCart(state){
-      state.cart = []
+    emptyCart(state) {
+      state.cart = [];
     },
-    updateItemInCart(state,id){
-      let index = state.cart.findIndex(item => item.id === id)
+    updateItemInCart(state, id) {
+      let index = state.cart.findIndex((item) => item.id === id);
       state.cart[index].quantity++;
-
     },
-    removeItemInCart(state,id){
-      let index = state.cart.findIndex(item => item.id===id)
-      state.cart.splice(index,1)
+    removeItemInCart(state, id) {
+      let index = state.cart.findIndex((item) => item.id === id);
+      state.cart.splice(index, 1);
     },
-    orderHistory(state,orderHistory){
-      state.orderHistory = orderHistory
-    }
+    orderHistory(state, orderHistory) {
+      state.orderHistory = orderHistory;
+    },
   },
   actions: {
-    async udateName(context,data){
-    await context.commit('login',data )
+    async udateName(context, data) {
+      await context.commit("login", data);
     },
 
-    async getMenuList(context){
-      let resp = await axios.get(API)
-       context.commit('displayMenu',resp.data.menu)
-      
+    async getMenuList(context) {
+      let resp = await axios.get(API);
+      context.commit("displayMenu", resp.data.menu);
     },
-    additemTocart(context,item){
-      let checkItem = context.state.cart.filter(check => check.id === item.id)
-      
-      if(checkItem.length > 0){
-        context.commit('updateItemInCart',checkItem[0].id)
-      }else{
-        context.commit('additem',item)} 
-    },
-    async sendOrder(context){
-      console.log("Order is send")
-      let order = {
-        items: context.state.cart
+    additemTocart(context, item) {
+      let checkItem = context.state.cart.filter(
+        (check) => check.id === item.id
+      );
+
+      if (checkItem.length > 0) {
+        context.commit("updateItemInCart", checkItem[0].id);
+      } else {
+        context.commit("additem", item);
       }
-      
-      let uuid = await localStorage.getItem('airbeans')
-    
-      try{
-        context.state.loading = true
-        let resp = await axios.post(`${API}/order/${uuid}`,order)
-        context.state.loading = false
-        context.commit('orderStatus', resp.data)
+    },
+    async sendOrder(context) {
+      console.log("Order is send");
+      let order = {
+        items: context.state.cart,
+      };
 
-      }catch(err){
-        console.error(err)
+      let uuid = await localStorage.getItem("airbeans");
+
+      try {
+        context.state.loading = true;
+        let resp = await axios.post(`${API}/order/${uuid}`, order);
+        context.state.loading = false;
+        context.commit("orderStatus", resp.data);
+      } catch (err) {
+        console.error(err);
       }
       //remove
-      context.state.cart = []
+      context.state.cart = [];
 
       //  empty cart
-       context.commit("emptyCart")
-    
-  },
-    async findUuid(){
-      try{
-        if(localStorage.getItem('airbeans') === null){
-          let uuid = await axios.get(`${API}/key`)
-          localStorage.setItem('airbeans', uuid.data.key)
+      context.commit("emptyCart");
+    },
+    async findUuid() {
+      try {
+        if (localStorage.getItem("airbeans") === null) {
+          let uuid = await axios.get(`${API}/key`);
+          localStorage.setItem("airbeans", uuid.data.key);
         }
-      } catch(err){
-        console.error(err)
-      } 
-      
-    },
-    async getOrders(context){
-      let uuid = await localStorage.getItem('airbeans')
-      try{
-        let resp = await axios.get(`${API}/profile/${uuid}`)
-        context.commit('orderHistory', resp.data)
-  
-      }catch(err){
-        console.error(err)
+      } catch (err) {
+        console.error(err);
       }
-  
-    }  
-  },
-  getters:{
-    cartItemCount(state){
-      let total= 0;
-       state.cart.forEach(item =>{
-        total += item.quantity;
-        });
-        return total;
     },
-    totalMountCart(state){
+    async getOrders(context) {
+      let uuid = await localStorage.getItem("airbeans");
+      try {
+        let resp = await axios.get(`${API}/profile/${uuid}`);
+        context.commit("orderHistory", resp.data);
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+  getters: {
+    cartItemCount(state) {
+      let total = 0;
+      state.cart.forEach((item) => {
+        total += item.quantity;
+      });
+      return total;
+    },
+    totalMountCart(state) {
       let totalamount = 0;
-      state.cart.forEach(item=>{
-      totalamount += item.price * item.quantity
-      })
-      return totalamount
-    }
-  }
-
-
-
-})
+      state.cart.forEach((item) => {
+        totalamount += item.price * item.quantity;
+      });
+      return totalamount;
+    },
+    totalAmount(state) {
+      let amount = 0;
+      amount += state.orderHistory
+        .map((item) => item.total)
+        .reduce((a, b) => {
+          return a + b;
+        }, 0);
+      return amount;
+    },
+  },
+});
