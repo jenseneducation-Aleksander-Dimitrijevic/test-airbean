9fad0a4d028f532a1e0acb0ab08c88b9a6f36b7a
maxaakre <maxaakre@gmail.com>
2020-08-27 14:42:54 +0200

    added public in api

--- api/app.js
+++ api/app.js
@@ -1,16 +1,20 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
 
-const beansRouter = require('./routes/beans')
-const PORT = 5000
+const beansRouter = require("./routes/beans");
+const PORT = 5000;
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/beans", beansRouter);
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/beans', beansRouter)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/public")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(_dirname, "/public", "index.html"));
+  });
+}
 
-
-app.listen(5000, () => console.log(`Server started on port ${PORT}`))
-
-
+app.listen(5000, () => console.log(`Server started on port ${PORT}`));
