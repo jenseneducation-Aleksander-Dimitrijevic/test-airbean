02cbb790bc045945288c5846189edff248dd2400
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-08-28 11:49:06 +0200

    init commit

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 
 const beansRouter = require("./routes/beans");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -13,8 +13,12 @@ app.use("/api/beans", beansRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/public")));
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(_dirname, "/public", "index.html"));
+    res.sendFile(path.join(__dirname, "/public", "index.html"));
   });
 }
 
-app.listen(5000, () => console.log(`Server started on port ${PORT}`));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- api/routes/beans.js
+++ api/routes/beans.js
@@ -1,58 +1,56 @@
-const { Router } = require('express')
-const router = new Router()
-const fs = require('fs');
-const uuid = require('uuid-random');
-const { generateOrderNr, generateETA } = require('../utils/utils');
-const DataStore = require('nedb-promise')
-
+const { Router } = require("express");
+const router = new Router();
+const fs = require("fs");
+const uuid = require("uuid-random");
+const { generateOrderNr, generateETA } = require("../utils/utils");
+const DataStore = require("nedb-promise");
 
 const Orderdata = new DataStore({
-    filename:'db/orders.db',
-    autoload:true
-})
-console.log(Orderdata)
-
-router.get('/', async (req, res) => {
-    const menu = fs.createReadStream('data/menu.json');
-    menu.pipe(res);
+  filename: "db/orders.db",
+  autoload: true,
 });
 
-router.post('/order/:uuid', async (req, res) => {
-
-    let uuid = req.params.uuid
-    const order = {
-        eta: generateETA(),
-        orderNr: generateOrderNr(),
-        items:req.body.items,
-        total: req.body.items.reduce((total,item) => total + item.price * item.quantity,0),
-        timeStamp: Date.now(),
-        id:uuid
-    }
-    const neworder = await Orderdata.insert(order)
-    console.log(neworder)
-
-
-    setTimeout(() => {
-        res.send(JSON.stringify(neworder));
-    }, 2000);
+router.get("/", async (req, res) => {
+  const menu = fs.createReadStream("data/menu.json");
+  menu.pipe(res);
 });
 
-router.get('/key', (req, res) => {
-    const key = {
-        key: uuid()
-    }
-    res.send(JSON.stringify(key));
-})
+router.post("/order/:uuid", async (req, res) => {
+  let uuid = req.params.uuid;
+  const order = {
+    eta: generateETA(),
+    orderNr: generateOrderNr(),
+    items: req.body.items,
+    total: req.body.items.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    ),
+    timeStamp: Date.now(),
+    id: uuid,
+  };
+  const neworder = await Orderdata.insert(order);
+  console.log(neworder);
 
+  setTimeout(() => {
+    res.send(JSON.stringify(neworder));
+  }, 2000);
+});
 
-router.get('/profile/:uuid', async (req,res) => {
-    const id=req.params.uuid
-    const getorders = await Orderdata.find({id:id})
-    ///hämta dokumentet med nyckel uuid i databasen
-    console.log(getorders)
+router.get("/key", (req, res) => {
+  const key = {
+    key: uuid(),
+  };
+  res.send(JSON.stringify(key));
+});
 
-    //Skicka tillbaka till frontend
-    res.status(200).send(JSON.stringify(getorders));
-})
+router.get("/profile/:uuid", async (req, res) => {
+  const id = req.params.uuid;
+  const getorders = await Orderdata.find({ id: id });
+  ///hämta dokumentet med nyckel uuid i databasen
+  console.log(getorders);
 
-module.exports = router
+  //Skicka tillbaka till frontend
+  res.status(200).send(JSON.stringify(getorders));
+});
+
+module.exports = router;
